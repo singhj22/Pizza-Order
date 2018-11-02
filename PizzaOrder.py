5cab4cc5bdfbdcc99f58af06bd457a24b95be42a
singhj22 <42975589+singhj22@users.noreply.github.com>
2018-11-02 12:47:38 -0400

    Add files via upload

---
+++ PizzaOrder.py
@@ -0,0 +1,21 @@
+'''
+Created on Nov 2, 2018
+
+@author: Jugat Singh
+Pizza Order
+'''
+
+gluten= input("Would you like gluten free pizza(Yes or No): ")
+pepperoni= input("Would you like pepperoni on your pizza(Yes or No): ")
+
+regular= 12.50
+gluten_p= 14.50
+pepperoni_p= 1.25
+
+if gluten.lower()== 'yes':
+    
+else:
+        if pepperoni.lower()== 'yes'
+            print(gluten_p+pepperoni_p)
+        else:
+            print(gluten_p)
